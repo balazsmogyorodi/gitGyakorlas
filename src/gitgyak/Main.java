e741ec8f16ad4acf99f8bbc16a5ecab005a40fbc
Mogyoródi Balázs (SZF_2022_2024) <mogyorodi.balazs@diak.szamalk-szalezi.hu>
2023-09-20 12:51:12 +0200

    elagazas

--- src/gitgyak/Main.java
+++ src/gitgyak/Main.java
@@ -4,7 +4,12 @@ package gitgyak;
 public class Main {
 
     public static void main(String[] args) {
-       
+        for (int i = 0; i < 10; i++) {
+            if(i == 1){
+                
+            }
+            
+        }
     }
     
 }
